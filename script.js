d94768aa7de98470776dae02d3bbffaf6af8340e
Adaline <72008645+hanna13n@users.noreply.github.com>
2021-03-29 17:00:17 +0530

    Adding Game Instructions

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ console.log(number)
 function randomguess() {
   var guess = (document.getElementById("userinput")).value
   if (number > guess)
-    alert("Your guess is too low!")
+    alert("Your guess is wrong.\n Next clue: The number is larger than your guess.")
   else if (number < guess)
-    alert("Your guess is too high!")
+    alert("Your guess is wrong.\n Next clue: The number is smaller than your guess.")
   else if (number = guess)
     alert("Hurray! you guessed right.")
 }
